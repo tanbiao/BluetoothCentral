d2dae6155899c591bba13e01964fb60c95d742cd
谭彪 <tanbiao@tanbiaodeMac-mini.local>
2017-10-19 10:05:14 +0800

    Initial Commit

---
+++ 蓝牙技术/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  蓝牙技术
+//
+//  Created by 谭彪 on 2017/10/19.
+//  Copyright © 2017年 谭彪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 蓝牙技术/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  蓝牙技术
+//
+//  Created by 谭彪 on 2017/10/19.
+//  Copyright © 2017年 谭彪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
